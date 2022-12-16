90fac47efc8f8c7b58a34ed9094cc8c1421525e8
Brandon Smith <smithb@wlwv.k12.or.us>
2022-12-16 08:22:00 -0800

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+    Code from https://horstmann.com/sjsu/graphics/
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle box = new Rectangle(5,10,60,80);
+        box.draw();
+        Ellipse egg = new Ellipse(100, 100, 40, 60);
+        egg.setColor(Color.YELLOW);
+        egg.fill();
+    }
+}
